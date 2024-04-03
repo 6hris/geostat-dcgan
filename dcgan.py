a49fff429fa6db4658501c004b6fefcecdaaf464
Christopher Dowdy <cdowdy81@gmail.com>
2024-04-03 01:16:35 -0400

    enabled checkpoints

--- dcgan.py
+++ dcgan.py
@@ -4,7 +4,6 @@ import os
 import numpy as np
 import math
 
-
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
@@ -15,41 +14,6 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-    
-# Load the data
-data_path = "GL_ensemble.parquet.gzip"
-image_df = pd.read_parquet(data_path)
-image_names = [f'sim_{i}' for i in range(1,4041)]
-
-realizations_resized = np.zeros((4020, 96, 96))
-
-for i in range(4020):
-    realization_og = np.array(image_df[image_names[i]].values).reshape(100,100)
-    tmp = realization_og[:96, :96]
-    realizations_resized[i] = tmp
-
-# Normalize the data to the range [-1, 1]
-realizations_normalized = (realizations_resized - realizations_resized.min()) / (realizations_resized.max() - realizations_resized.min()) * 2 - 1
-
-
-os.makedirs("images", exist_ok=True)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=96, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
-opt = parser.parse_args()
-print(opt)
-
-cuda = True if torch.cuda.is_available() else False
-
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -61,11 +25,11 @@ def weights_init_normal(m):
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, img_size, latent_dim, channels):
         super(Generator, self).__init__()
 
-        self.init_size = opt.img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, 128 * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
@@ -77,7 +41,7 @@ class Generator(nn.Module):
             nn.Conv2d(128, 64, 3, stride=1, padding=1),
             nn.BatchNorm2d(64, 0.8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
+            nn.Conv2d(64, channels, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
@@ -119,86 +83,133 @@ class Discriminator(nn.Module):
 
         return validity
 
+if __name__ == "__main__":
+    # Load the data
+    data_path = "GL_ensemble.parquet.gzip"
+    image_df = pd.read_parquet(data_path)
+    image_names = [f'sim_{i}' for i in range(1,4041)]
 
-# Loss function
-adversarial_loss = torch.nn.BCELoss()
+    realizations_resized = np.zeros((4020, 96, 96))
 
-# Initialize generator and discriminator
-generator = Generator()
-discriminator = Discriminator()
+    for i in range(4020):
+        realization_og = np.array(image_df[image_names[i]].values).reshape(100,100)
+        tmp = realization_og[:96, :96]
+        realizations_resized[i] = tmp
 
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-    adversarial_loss.cuda()
+    # Normalize the data to the range [-1, 1]
+    realizations_normalized = (realizations_resized - realizations_resized.min()) / (realizations_resized.max() - realizations_resized.min()) * 2 - 1
 
-# Initialize weights
-generator.apply(weights_init_normal)
-discriminator.apply(weights_init_normal)
 
-# Configure data loader
-realizations_tensor = torch.tensor(realizations_normalized, dtype=torch.float).unsqueeze(1)
-dataset = TensorDataset(realizations_tensor)
-dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
+    os.makedirs("images", exist_ok=True)
+    os.makedirs("checkpoints", exist_ok=True)
 
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+    parser.add_argument("--img_size", type=int, default=96, help="size of each image dimension")
+    parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+    opt = parser.parse_args()
+    print(opt)
 
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+    cuda = True if torch.cuda.is_available() else False
 
-# ----------
-#  Training
-# ----------
+    # Loss function
+    adversarial_loss = torch.nn.BCELoss()
 
-for epoch in range(opt.n_epochs):
-    for i, batch in enumerate(dataloader):
+    # Initialize generator and discriminator
+    generator = Generator(img_size=opt.img_size, latent_dim=opt.latent_dim, channels=opt.channels)
+    discriminator = Discriminator()
 
-        imgs = batch[0]
-        # Adversarial ground truths
-        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
-        fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+    if cuda:
+        generator.cuda()
+        discriminator.cuda()
+        adversarial_loss.cuda()
 
-        # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
+    # Initialize weights
+    generator.apply(weights_init_normal)
+    discriminator.apply(weights_init_normal)
 
-        # -----------------
-        #  Train Generator
-        # -----------------
+    # Configure data loader
+    realizations_tensor = torch.tensor(realizations_normalized, dtype=torch.float).unsqueeze(1)
+    dataset = TensorDataset(realizations_tensor)
+    dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
 
-        optimizer_G.zero_grad()
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-        # Generate a batch of images
-        gen_imgs = generator(z)
+    # ----------
+    #  Training
+    # ----------
 
-        # Loss measures generator's ability to fool the discriminator
-        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+    for epoch in range(opt.n_epochs):
+        for i, batch in enumerate(dataloader):
 
-        g_loss.backward()
-        optimizer_G.step()
+            imgs = batch[0]
+            # Adversarial ground truths
+            valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
+            # Configure input
+            real_imgs = Variable(imgs.type(Tensor))
 
-        optimizer_D.zero_grad()
+            # -----------------
+            #  Train Generator
+            # -----------------
 
-        # Measure discriminator's ability to classify real from generated samples
-        real_loss = adversarial_loss(discriminator(real_imgs), valid)
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2
+            optimizer_G.zero_grad()
 
-        d_loss.backward()
-        optimizer_D.step()
+            # Sample noise as generator input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
 
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
+            # Generate a batch of images
+            gen_imgs = generator(z)
 
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+            # Loss measures generator's ability to fool the discriminator
+            g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+            g_loss.backward()
+            optimizer_G.step()
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            optimizer_D.zero_grad()
+
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = adversarial_loss(discriminator(real_imgs), valid)
+            fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            )
+
+            batches_done = epoch * len(dataloader) + i
+            if batches_done % opt.sample_interval == 0:
+                for index, image in enumerate(gen_imgs.data[:25]):
+                    save_path = f"images/{batches_done}_{index}.png"
+                    save_image(image, save_path, normalize=True)
+
+            if epoch % 10 == 0: 
+                torch.save({
+                    'epoch': epoch,
+                    'generator_state_dict': generator.state_dict(),
+                    'discriminator_state_dict': discriminator.state_dict(),
+                    'optimizer_G_state_dict': optimizer_G.state_dict(),
+                    'optimizer_D_state_dict': optimizer_D.state_dict(),
+                }, f"checkpoints/checkpoint_epoch_{epoch}.pth")
+            
--- dcgan_analysis.py
+++ dcgan_analysis.py
@@ -1,25 +1,100 @@
 from PIL import Image
 import os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from dcgan import Generator
+import torch
 
-def calculations(image_folder):
-    # calculate mean and variance
-    means = []
-    variances = []
+def main():
+    checkpoint = 10
+    checkpoint_path = "test_checkpoints/checkpoint_epoch_10.pth"
 
-    for name in os.listdir(image_folder):
-        path = os.path.join(image_folder, name)
-        image = Image.open(path).convert('L')
-        image_normalized = np.array(image, dtype=np.float32) / 255.0
+    sgs_df = pd.read_parquet("GL_ensemble.parquet.gzip", engine = "fastparquet")
+    dcgan_df = dcgan_sim(checkpoint_path, 25)
 
-        mean = np.mean(image_normalized)
-        variance = np.var(image_normalized)
+    print("Begin")
 
-        means.append(mean)
-        variances.append(variance)
+    coords = sgs_df[['X','Y']].values.reshape(100,100,2)
+    coords = coords[:96,:96].reshape(9216,2)
 
-    return np.mean(means), np.mean(variances)
+    sgs_topo = load_sgs_sim(sgs_df, 25)
 
-image_folder = 'images'
-avg_mean, avg_variance = calculations(image_folder)
-print(f"Average Mean: {avg_mean}, Average Variance: {avg_variance}")
+    for i in range(25):
+        ele = dcgan_df[:,i]
+        norm_ele = (ele - 0.5) * 2
+        dcgan_df[:,i] = norm_ele
+    
+    # Variance of dcgan samples at each location
+    dcgan_var = np.mean(dcgan_df, axis=0)
+    print(dcgan_var.shape)
+
+    # Variance of SGS samples at each location
+    SGS_var = np.mean(sgs_topo, axis=0)
+    print(SGS_var.shape)
+
+    # Variance of SGS data
+    plot_colormap(coords[:,0], coords[:,1], SGS_var, "Normalized Mean Ele", 
+                       "SGS Elevation", checkpoint, "SGS_mean.png")
+
+    # Variance of DCGAN data
+    plot_colormap(coords[:,0], coords[:,1], dcgan_var, "Normalized Mean Ele", 
+                   f"DCGAN Elevation CP {checkpoint}", checkpoint, "DCGAN_mean.png")
+
+    # Difference map of Variance
+    plot_colormap(coords[:,0], coords[:,1],SGS_var - dcgan_var, "Difference Norm Mean Ele", 
+                    f"SGS - DCGAN Elevation CP {checkpoint}", checkpoint, "diff_mean.png")
+
+
+def dcgan_sim(checkpoint_path, num_images):
+    generator = Generator(96, 100, 1) # default parameters
+    generator.eval()
+
+    checkpoint = torch.load(checkpoint_path, map_location='cpu')
+    generator.load_state_dict(checkpoint['generator_state_dict'])
+
+    z = torch.randn(num_images, 100) 
+    with torch.no_grad():
+        generated_images = generator(z)
+    
+    return generated_images.cpu().numpy().reshape(num_images, -1)
+
+def load_sgs_sim(img_df, num_images):
+
+    image_df = pd.read_parquet("GL_ensemble.parquet.gzip", engine = "fastparquet")
+    image_names = [f'sim_{i}' for i in range(1,4041)]
+
+    rand_sim = np.random.randint(0, high=4041, size=num_images)
+
+    tmp = np.zeros((num_images,100,100), dtype='float')
+    sgs_sim = np.zeros((num_images,9216), dtype='float')
+
+    for i, n in enumerate(rand_sim):
+        tmp[i] = np.array(img_df[image_names[n]].values).reshape(100,100)
+        sgs_sim[i] = tmp[i,:96,:96].reshape(9216)
+
+    return sgs_sim
+
+def plot_colormap(X, Y, Color, colorbar_title, title, cp, filename):
+
+    fig = plt.figure(figsize = (6,5))
+    ax = plt.gca()
+    im = ax.scatter(X, Y, c=Color, marker='.', s=5, cmap='gist_earth')
+    plt.title(title)
+    plt.xlabel('X [m]'); plt.ylabel('Y [m]')
+    plt.locator_params(nbins=5)
+    plt.axis('scaled')
+
+    # make colorbar
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.1)
+    cbar = plt.colorbar(im, ticks=np.linspace(np.min(Color), np.max(Color), 11), cax=cax)
+    cbar.set_label(colorbar_title, rotation=270, labelpad=15)
+    save_dir = f'compare/cp_{cp}'
+    os.makedirs(save_dir, exist_ok=True)
+    
+    fig.savefig(os.path.join(save_dir, filename), dpi=fig.dpi)
+
+if __name__ == "__main__":
+    main()
